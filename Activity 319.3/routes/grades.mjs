510f01ed26bc015d982b9366f42c994612563da0
Kate Iliu <beviatori@gmail.com>
2024-04-26 14:46:20 -0600

    Added more methods in Actvity319

--- Activity 319.3/routes/grades.mjs
+++ Activity 319.3/routes/grades.mjs
@@ -87,4 +87,20 @@ router.patch("/:id/remove", async (req, res) => {
     else res.send(result).status(200);
   });
 
+  // Get route for backwards compatibility
+router.get("/student/:id", async (req, res) => {
+    res.redirect(`learner/${req.params.id}`);
+  });
+
+  // Delete a learner's grade data
+router.delete("/learner/:id", async (req, res) => {
+    let collection = await db.collection("grades");
+    let query = { learner_id: Number(req.params.id) };
+  
+    let result = await collection.deleteOne(query);
+  
+    if (!result) res.send("Not found").status(404);
+    else res.send(result).status(200);
+  });
+
 export default router
